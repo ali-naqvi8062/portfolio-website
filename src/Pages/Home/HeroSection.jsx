ffe17fc91a7128e4674e94f923b9138b5db66f77
Ali Naqvi <naqvia13@mcmaster.ca>
2024-04-25 17:31:52 -0400

    Add hero section basic items

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,3 +1,24 @@
 export default function HeroSection () {
-    return <h1>HeroSection</h1>;
+    return(
+      <section id="heroSection" className="hero--section">
+        <div className="hero--section--content--box">
+            <div className="hero--section--content">
+                <p className="section--title">Hey, I'm Ali!</p>
+                <h1 className="hero--section--title">
+                    <span className="hero--section--title--color">Full Stack</span>{" "}
+                    <br />
+                    Developer
+                </h1>
+                <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                <br/> Sunt fuga neque aspernatur obcaecati pariatur.
+                </p>
+            </div>
+            <button className="btn btn-primary">Get in Touch</button>
+
+        </div>
+        <div className="hero--section--img">
+            <img src="./img/hero_img.png" alt="Hero Section" />
+        </div>
+      </section>  
+    );
 }

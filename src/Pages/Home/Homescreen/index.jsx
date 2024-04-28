98b06e4f1273b44c64d132d0f302247bf7c04464
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-04-28 18:12:34 -0400

    Add skills section

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../HeroSection";
+import Skills from "../Skills";
 
 export default function Home () {
     return(
         <>
             <HeroSection />
+            <Skills />
         </>
     );
 }

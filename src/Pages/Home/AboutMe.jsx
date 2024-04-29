1d55927249c49af40779df9ee5d7d838340c8a4b
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-04-29 15:22:44 -0400

    Create About Me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,30 @@
+export default function AboutMe () {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">
+                        About
+                    </p>
+                    <h1 className="skills-section--heading">
+                        About Me
+                    </h1>
+                    <p className="hero--section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dignissimos veritatis quis consequatur et 
+                        architecto recusandae vero asperiores vitae, reprehenderit error amet libero, iusto facere blanditiis quia ab. 
+                        Rem, exercitationem obcaecati mollitia non eveniet quis corrupti, voluptatibus aliquid recusandae minima, 
+                        delectus esse pariatur libero dolore deserunt optio! 
+                        Fugiat alias fuga iusto, tempore exercitationem consectetur placeat praesentium repellat, amet esse sint!
+                    </p>
+                    <p className="hero--section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit error, corporis saepe possimus esse nisi.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit error, corporis saepe possimus esse nisi.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}

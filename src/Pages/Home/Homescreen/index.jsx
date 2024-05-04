dbb4d2b5f8137427a192db90de7c6d37f530ddb2
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-05-03 21:16:57 -0400

    fixed button click and page refresh bug, replaced skills and portfolio sections with featured and projects description sections

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -2,17 +2,16 @@ import AboutMe from "../AboutMe";
 import ContactMe from "../ContactMe";
 import Footer from "../Footer";
 import HeroSection from "../HeroSection";
-import MyPortfolio from "../MyPortfolio";
-import Skills from "../Skills";
-import Testimonial from "../Testimonials";
+import Featured from "../Featured";
+import MyPortfolio from "../Portfolio";
 
 export default function Home () {
     return(
         <>
             <HeroSection />
-            <Skills />
+            <Featured />
             <AboutMe />
-            <MyPortfolio />
+            <MyPortfolio />            
             <ContactMe />
             <Footer />
         </>

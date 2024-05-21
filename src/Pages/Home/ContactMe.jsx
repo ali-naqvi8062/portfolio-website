b902c406ba9ea1f91432acca9694db30f824a5d9
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-05-21 16:13:03 -0400

    Route form submission to email using EmailJS

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,74 +1,90 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+
 export default function ContactMe() {
-    return (
-      <section id="Contact" className="contact--section">
-        <div>
-          <h2>Contact Me</h2>
-        </div>
-        <form className="contact--form--container">
-          <div className="container">
-            <label htmlFor="first-name" className="contact--label">
-              <span className="text-md">First Name</span>
-              <input
-                type="text"
-                className="contact--input text-md"
-                name="first-name"
-                id="first-name"
-                required
-              />
-            </label>
-            <label htmlFor="last-name" className="contact--label">
-              <span className="text-md">Last Name</span>
-              <input
-                type="text"
-                className="contact--input text-md"
-                name="last-name"
-                id="last-name"
-                required
-              />
-            </label>
-            <label htmlFor="email" className="contact--label">
-              <span className="text-md">Email</span>
-              <input
-                type="email"
-                className="contact--input text-md"
-                name="email"
-                id="email"
-                required
-              />
-            </label>
-            <label htmlFor="phone-number" className="contact--label">
-              <span className="text-md">phone-number</span>
-              <input
-                type="number"
-                className="contact--input text-md"
-                name="phone-number"
-                id="phone-number"
-                required
-              />
-            </label>
-          </div>
-          <label htmlFor="choode-topic" className="contact--label">
-            <span className="text-md">Choose a topic</span>
-            <select id="choose-topic" className="contact--input text-md">
-              <option>Select One...</option>
-              <option>Item 1</option>
-              <option>Item 2</option>
-              <option>Item 3</option>
-            </select>
-          </label>
-          <label htmlFor="message" className="contact--label">
-            <span className="text-md">Message</span>
-            <textarea
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    emailjs.sendForm('service_aajdtdt', 'template_y4xkobs', event.target, '2WMAlBJgCkwxNeaYr')
+      .then((result) => {
+        console.log(result.text);
+        alert('Email sent successfully!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send email.');
+      });
+  };
+
+  return (
+    <section id="Contact" className="contact--section">
+      <div>
+        <h2>Contact Me</h2>
+      </div>
+      <form className="contact--form--container" onSubmit={handleSubmit}>
+        <div className="container">
+          <label htmlFor="first-name" className="contact--label">
+            <span className="text-md">First Name</span>
+            <input
+              type="text"
               className="contact--input text-md"
-              id="message"
-              rows="8"
-              placeholder="Type your message..."
+              name="firstName"
+              id="first-name"
+              required
             />
           </label>
-          <div>
-            <button className="btn btn-primary contact--form--btn">Submit</button>
-          </div>
-        </form>
-      </section>
-    );
-  }
+          <label htmlFor="last-name" className="contact--label">
+            <span className="text-md">Last Name</span>
+            <input
+              type="text"
+              className="contact--input text-md"
+              name="lastName"
+              id="last-name"
+              required
+            />
+          </label>
+          <label htmlFor="email" className="contact--label">
+            <span className="text-md">Email</span>
+            <input
+              type="email"
+              className="contact--input text-md"
+              name="email"
+              id="email"
+              required
+            />
+          </label>
+          <label htmlFor="phone-number" className="contact--label">
+            <span className="text-md">Phone Number</span>
+            <input
+              type="number"
+              className="contact--input text-md"
+              name="phoneNumber"
+              id="phone-number"
+            />
+          </label>
+        </div>
+        <label htmlFor="choose-topic" className="contact--label">
+          <span className="text-md">Choose a topic</span>
+          <select id="choose-topic" className="contact--input text-md" name="topic">
+            <option>Select One...</option>
+            <option>Resume/Credentials Info</option>
+            <option>General Inquiry</option>
+            <option>Other</option>
+          </select>
+        </label>
+        <label htmlFor="message" className="contact--label">
+          <span className="text-md">Message</span>
+          <textarea
+            className="contact--input text-md"
+            id="message"
+            name="message"
+            rows="8"
+            placeholder="Type your message..."
+          />
+        </label>
+        <div>
+          <button type="submit" className="btn btn-primary contact--form--btn">Submit</button>
+        </div>
+      </form>
+    </section>
+  );
+}

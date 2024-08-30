6a9546c73db22187efebff0e5ad5bb202a386528
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-08-30 17:30:30 -0400

    Update font size for About Me heading

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -6,17 +6,17 @@ export default function AboutMe () {
             </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
-                    <h1 className="skills-section--heading">
+                    <h1 className="skills-section--heading" id="abt-head">
                         About Me
                     </h1>
-                    <p className="hero--section-description">
+                    <p className="hero--section-description" id="abt">
                         I am an aspiring Computer Engineer based in the Greater Toronto Area with a passion for using technology to 
                         bring ideas to life. My portfolio showcases a variety of projects I've worked on which
                         include embedded systems projects, game development, and web development which are areas I am primarily focused in. 
                         I thrive on the challenge of turning concepts into reality and my work reflects my commitment to exploring the 
                         intersection of creativity and technology.
                     </p>
-                    <p className="hero--section-description">
+                    <p className="hero--section-description" id="abt">
                         Currently I am studying in my 3rd year of Computer Engineering at McMaster University.
                         In my free time I enjoy playing competitive chess, video games and playing soccer. 
                     </p>
